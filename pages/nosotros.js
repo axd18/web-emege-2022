a0f83d1a5064e9d8b0a865c7a54ed984b7b8dcea
Adrian <adrianfernandezj@gmail.com>
2021-03-29 12:54:38 -0300

    first commit

---
+++ pages/nosotros.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Layout from '../components/layout/Layout';
+
+const Index = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>Desde Nostros</h1>
+      </Layout>
+    </div>
+  );
+}
+
+export default Index;
