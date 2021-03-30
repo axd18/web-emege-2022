9b7541183779a5db11d7834d8c3eb403c2db29f1
Adrian <adrianfernandezj@gmail.com>
2021-03-30 12:54:22 -0300

    update

---
+++ components/main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Main = () => {
+    
+    return ( 
+        <div className=""> 
+            <img src="/img-main-euro.jpg"/>
+        </div>
+     );
+}
+ 
+export default Main;
