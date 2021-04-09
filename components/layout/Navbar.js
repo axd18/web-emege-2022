4a97fc6d3d7d4d5be0f180c4a6c6799427c2d281
Adrian <adrianfernandezj@gmail.com>
2021-04-09 12:10:44 -0300

    update

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -8,14 +8,11 @@ const Navbar = () => {
     return ( 
         
         <header>
-            <nav className="bg-gray-600  absolute w-full items-center justify-center px-2 md:bg-opacity-0">
+            <nav className="bg-gray-400 absolute w-full items-center justify-center px-2 md:bg-opacity-0">
                 <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
                     <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-                        <div
-                        className="inline-block mr-4 py-2 whitespace-no-wrap object-contain w-20 pt-4 cursor-pointer"
-                        >
-                        {/* <img src={logo} alt="logo"/> */}
-                        <h1>Emege</h1>
+                        <div className="inline-block mr-4  content-start whitespace-no-wrap object-contain w-20 md:w-36 cursor-pointer">
+                            <img src="/logo-emege.png"/>
                         </div>
                         <button
                         className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent  block lg:hidden outline-none focus:outline-none"
@@ -32,26 +29,26 @@ const Navbar = () => {
                     }
                     id="example-navbar-danger"
                 >
-                    <ul className="flex flex-col lg:flex-row list-none lg:ml-auto ">
-                    <li className="nav-item">
+                    <ul className="flex flex-col lg:flex-row list-none lg:ml-auto font-semibold">
+                    <li className="nav-item pr-10">
                         <Link href="/"
-                            className="px-3 py-2 flex items-center text-lg leading-snug text-white hover:opacity-75 cursor-pointer"
+                            className=" py-2 flex items-center text-lg leading-snug text-white hover:opacity-75 cursor-pointer"
                             >Línea Patagonia
                         </Link>
                     </li>
-                    <li className="nav-item">
+                    <li className="nav-item pr-10">
                         <Link href="/"
                             className="px-3 py-2 flex items-center text-lg text-white hover:opacity-75 cursor-pointer"
                             >Línea Euro
                         </Link>
                     </li>
-                    <li className="nav-item">
+                    <li className="nav-item pr-10">
                         <Link href="/"
                         className="px-3 py-2 flex items-center text-lg text-white hover:opacity-75 cursor-pointer"
                         >Termotanques
                         </Link>
                     </li>
-                    <li className="nav-item">
+                    <li className="nav-item pr-10">
                         <Link href="/"
                             className="px-3 py-2 flex items-center text-lg text-white hover:opacity-75 cursor-pointer"
                             >Asistencia
--- pages/index.js
+++ pages/index.js
@@ -19,30 +19,78 @@ const Index = () => {
       {/* <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p> */}
     </div>
             <div class="flex flex-wrap -m-4">
-            <div class="p-4 md:w-1/3">
-            <div class="h-full rounded-lg overflow-hidden shadow-md">
-              <img class="lg:h-auto md:h-auto w-auto object-cover object-center" src="/../img/9020.jpg" alt="blog"/>
-                <div class="p-6 bg-white">
-                  {/* <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}
-                  <h1 class="title-font text-3xl font-bold text-gray-900 mb-3">TB 1900 Kcal.</h1>
-                  <ul class="leading-relaxed mb-3">
-                    <li class="font-medium">Área a calefaccionar: </li>
-                    <li>14 m2. <span className="text-xs">(Altura de techo hasta 2,7 m aprox.)</span></li>
-                    <li class="font-medium">Medidas:</li>
-                    <li>Altura: 61 cm</li>
-                    <li>Ancho: 31 cm</li>
-                    <li>Prof.: 12 cm</li>
-                  </ul>
-                  <div class="flex items-center flex-wrap ">
-                  <button class="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded inline-flex items-center">
-                  <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
-                  <span>Descargar manual</span>
-                  </button>
+              <div class="p-4 md:w-1/3">
+                <div class="h-full rounded-lg overflow-hidden shadow-md">
+                  <img class="lg:h-auto md:h-auto w-auto object-cover object-center" src="/../img/9020.jpg" alt="blog"/>
+                  <div class="p-6 bg-white">
+                    {/* <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}
+                    <h1 class="title-font text-3xl font-bold text-gray-900 mb-3">TB 1900 Kcal.</h1>
+                    <ul class="leading-relaxed mb-3">
+                      <li class="font-medium">Área a calefaccionar: </li>
+                      <li>14 m2. <span className="text-xs">(Altura de techo hasta 2,7 m aprox.)</span></li>
+                      <li class="font-medium">Medidas:</li>
+                      <li>Altura: 61 cm</li>
+                      <li>Ancho: 31 cm</li>
+                      <li>Prof.: 12 cm</li>
+                    </ul>
+                    <div class="flex items-center flex-wrap ">
+                    <button class="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded inline-flex items-center">
+                    <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+                    <span>Descargar manual</span>
+                    </button>
+                    </div>
                   </div>
                 </div>
-            </div>
-            </div>
-            
+              </div>
+
+              <div class="p-4 md:w-1/3">
+                <div class="h-full rounded-lg overflow-hidden shadow-md">
+                  <img class="lg:h-auto md:h-auto w-auto object-cover object-center" src="/../img/9020.jpg" alt="blog"/>
+                  <div class="p-6 bg-white">
+                    {/* <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}
+                    <h1 class="title-font text-3xl font-bold text-gray-900 mb-3">TB 1900 Kcal.</h1>
+                    <ul class="leading-relaxed mb-3">
+                      <li class="font-medium">Área a calefaccionar: </li>
+                      <li>14 m2. <span className="text-xs">(Altura de techo hasta 2,7 m aprox.)</span></li>
+                      <li class="font-medium">Medidas:</li>
+                      <li>Altura: 61 cm</li>
+                      <li>Ancho: 31 cm</li>
+                      <li>Prof.: 12 cm</li>
+                    </ul>
+                    <div class="flex items-center flex-wrap ">
+                    <button class="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded inline-flex items-center">
+                    <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+                    <span>Descargar manual</span>
+                    </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="p-4 md:w-1/3">
+                <div class="h-full rounded-lg overflow-hidden shadow-md">
+                  <img class="lg:h-auto md:h-auto w-auto object-cover object-center" src="/../img/9020.jpg" alt="blog"/>
+                  <div class="p-6 bg-white">
+                    {/* <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}
+                    <h1 class="title-font text-3xl font-bold text-gray-900 mb-3">TB 1900 Kcal.</h1>
+                    <ul class="leading-relaxed mb-3">
+                      <li class="font-medium">Área a calefaccionar: </li>
+                      <li>14 m2. <span className="text-xs">(Altura de techo hasta 2,7 m aprox.)</span></li>
+                      <li class="font-medium">Medidas:</li>
+                      <li>Altura: 61 cm</li>
+                      <li>Ancho: 31 cm</li>
+                      <li>Prof.: 12 cm</li>
+                    </ul>
+                    <div class="flex items-center flex-wrap ">
+                    <button class="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded inline-flex items-center">
+                    <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+                    <span>Descargar manual</span>
+                    </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
             </div>
           </div>
         </section>
