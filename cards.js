c12cb437d4c34a0b6af33b2aa7b26eda84aeee59
Adrian <adrianfernandezj@gmail.com>
2021-04-12 13:08:48 -0300

    Hice un .map de las cards de termos

---
+++ cards.js
@@ -0,0 +1,22 @@
+export const termos = [
+    {
+        title: 'Recuperación simultánea 120',
+        aislacion: 'Aislación: Poliuretano expandido',
+        potencia: 'potencia: 4500',
+        tiraje: 'Tipo de tiraje: TN',
+    },
+    {
+        title: 'Recuperación simultánea 80',
+        aislacion: 'Aislación: Poliuretano expandido',
+        potencia: 'potencia: 4500',
+        tiraje: 'Tipo de tiraje: TN',
+        image: 'url:()'
+    },
+    {
+        title: 'Recuperación simultánea 80',
+        aislacion: 'Aislación: Poliuretano expandido',
+        potencia: 'potencia: 4500',
+        tiraje: 'Tipo de tiraje: TN',
+        image: 'url:()'
+    }
+]
