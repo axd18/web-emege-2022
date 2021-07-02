2ca9a9062b9b262c4c454dc8f8c7c8f1d82103d3
Adrian <adrianfernandezj@gmail.com>
2021-07-02 11:37:24 -0300

    Google Analytics

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -16,7 +16,18 @@ const Layout = ({children, header = true}) => {
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"/>
                 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"/>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
-                {/* <link rel="stylesheet" href="../package/swiper-bundle.min.css"></link> */}
+                <script
+                    dangerouslySetInnerHTML={{
+                    __html: `
+                        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136951460-1"></script>
+                        <script>
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-6DGYXH2YFD');
+                        </script>`
+                    }}
+                />
             </Head>
             {
                 header && (
