d551f88b0daca36dda672e530cf28670a86e82eb
Adrian <adrianfernandezj@gmail.com>
2021-07-07 09:39:06 -0300

    Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,23 @@ import 'swiper/swiper.scss';
 import '../global.css';
 import '../assets/css/secciones.css'
 
-function MyApp({ Component, pageProps }) {
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
+
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
